7e5c8be4da4562204ce361370ad2584d27caa032
fahmifauzi26 <fahmifauziabdullah26@gmail.com>
2022-07-04 13:37:43 +0700

    new update

--- data/slide46.js
+++ data/slide46.js
@@ -1,4 +1,4 @@
-(function(){var loadHandler=window['sl_{AA9AEF39-89AF-4013-AD8B-CF45DBBE57D1}'];loadHandler&&loadHandler(45, '<div id="spr0_30ed75a3"><div id="spr1_30ed75a3" class="kern slide"><img id="img1_30ed75a3" src="data/img3.png" width="595px" height="842px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_30ed75a3" class="kern slide"><div id="svg0_30ed75a3" style="left:47.141px;top:74.08px;"><svg width="501" height="622" viewBox="-0.75 -0.75 501 622"><path fill="none" stroke="#ffc000" stroke-width="1.5" stroke-linecap="butt" stroke-linejoin="round" d="M0.75,0.75 h497.718 v618.785 h-497.718 Z"/></svg></div><div id="spr3_30ed75a3" style="left:60px;top:90px;"><img id="img0_30ed75a3" src="data/img36.png" width="433" height="596" alt="Misal: 𝑥 = 𝑈𝑠𝑖𝑎\
+(function(){var loadHandler=window['sl_{E19FF4A7-D6D4-44DB-A8B7-A94480181650}'];loadHandler&&loadHandler(45, '<div id="spr0_d9c33cf"><div id="spr1_d9c33cf" class="kern slide"><img id="img1_d9c33cf" src="data/img3.png" width="595px" height="842px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_d9c33cf" class="kern slide"><div id="svg0_d9c33cf" style="left:47.141px;top:74.08px;"><svg width="501" height="622" viewBox="-0.75 -0.75 501 622"><path fill="none" stroke="#ffc000" stroke-width="1.5" stroke-linecap="butt" stroke-linejoin="round" d="M0.75,0.75 h497.718 v618.785 h-497.718 Z"/></svg></div><div id="spr3_d9c33cf" style="left:60px;top:90px;"><img id="img0_d9c33cf" src="data/img36.png" width="433" height="596" alt="Misal: 𝑥 = 𝑈𝑠𝑖𝑎\
 𝐴𝑛𝑎𝑘 𝑘𝑒 1 = 𝑥+20\
 𝐴𝑛𝑎𝑘 𝑘𝑒 2 = 𝑥+15\
 𝐴𝑛𝑎𝑘 𝑘𝑒 3 = 𝑥+10\
@@ -12,4 +12,4 @@
 100−50=5𝑥\
 𝑥= 50 5 \
 𝑥=10\
-Jadi, usia adik Aldo yang paling muda adalah 10 tahun."/></div><div id="spr4_30ed75a3" style="left:523.58px;top:783.278px;"><div style="width:0px;"><span id="txt0_30ed75a3" data-width="20.002970" style="left:18.413px;top:9.686px;">42</span></div></div></div></div>', '{"s":[]}');})();
+Jadi, usia adik Aldo yang paling muda adalah 10 tahun."/></div><div id="spr4_d9c33cf" style="left:523.58px;top:783.278px;"><div style="width:0px;"><span id="txt0_d9c33cf" data-width="20.002970" style="left:18.413px;top:9.686px;">41</span></div></div></div></div>', '{"s":[]}');})();
